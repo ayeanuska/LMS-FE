4eb622e5cd1883ade318cc734c0d4fa968644e82
Anuska <anuskasth2022@gmail.com>
2025-03-04 12:10:16 +1100

    ui chnages

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <Navbar expand="md" className="bg-dark  text-white" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">LMS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
--- src/components/layouts/Hero.jsx
+++ src/components/layouts/Hero.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 
 const Hero = () => {
-    
   return (
-    <div className="text-center py-5 bg-primary text-white">
-      Your content here
+    <div className="text-center py-4 bg-white text-dark">
       <h2 className="display-4 fw-bold mb-4">Welcome to the Library</h2>
-      <p className="fs-5">
-        Manage books, track records, and enhance the reading experience.
-      </p>
+      <p className="fs-5">Enhance the reading experience.</p>
     </div>
   );
 };
