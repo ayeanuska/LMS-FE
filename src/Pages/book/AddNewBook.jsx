90896c7a08fd5069205f5e474ac2d28a6a877c40
Anuska <anuskasth2022@gmail.com>
2025-03-11 21:55:09 +1100

    add new book function completed

---
+++ src/Pages/book/AddNewBook.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserLayout } from "../../components/layouts/UserLayout";
+import { useDispatch } from "react-redux";
+import { Button, Form } from "react-bootstrap";
+import CustomInput from "../../components/custom-input/CustomInput";
+import useForm from "../../hooks/useForm";
+import { bookInputes } from "../../assets/form-data/BookAuthInput";
+
+const AddNewBook = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { form, handleOnChange } = useForm(initialState);
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    //1. call post new book api
+    //2.status check
+    // call create new book action
+
+    const success = await dispatch(postNewBookAction(form));
+    if (success) {
+      navigate("/admin/books");
+    }
+  };
+
+  return (
+    <UserLayout pageTittle="New Book">
+      <div>
+        <Link to="/admin/books">
+          <Button variant="secondary">&lt; Back </Button>
+        </Link>
+      </div>
+
+      <div className="mt-5">
+        {/* form to add new books */}
+
+        <h4 className="py-4"> Add new book Here </h4>
+        <Form onSubmit={handleOnSubmit}>
+          {bookInputes.map((input, i) => (
+            <CustomInput
+              key={i}
+              {...input}
+              onChange={handleOnChange}
+            ></CustomInput>
+          ))}
+
+          <div className="d-grid">
+            <Button type="submit"> submit new book </Button>
+          </div>
+        </Form>
+      </div>
+    </UserLayout>
+  );
+};
+
+export default AddNewBook;
